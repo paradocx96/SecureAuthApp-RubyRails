a7e7a3e5bb074e61b1a9bc4a3e7f7ad1a67af835
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-20 17:01:32 +0530

    Current User Global usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :authorized?, :current_user_info
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def authorized?
+    !!current_user
+  end
+
+  def require_auth
+    redirect_to sign_in_path, alert: 'Please Signed in!' unless authorized?
+  end
 end
