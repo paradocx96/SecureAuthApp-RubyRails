911d7f8953362b842688b13da28e224f060754c0
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-20 17:00:27 +0530

    Login Controller

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,20 @@
+class LoginController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'User Successfully Logged!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logout Successfully!'
+  end
+end
