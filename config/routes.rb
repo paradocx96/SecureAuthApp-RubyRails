a559d1c766951ecef947b46639f3cad7d3991778
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-20 17:02:45 +0530

    Routes Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # Login Routes
+  get 'sign_in', to: 'login#new'
+  post 'sign_in', to: 'login#create'
 
+  # Logout Route
+  delete 'sign_out', to: 'login#destroy'
+
+  # Registration Routes
+  get 'sign_up', to: 'registration#new'
+  post 'sign_up', to: 'registration#create'
+
+  # Root Route
   root 'home#index'
 end
