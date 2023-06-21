22a66854a1d40fafdd7bb6b934fc23543927db75
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-21 11:12:43 +0530

    Book Validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates_presence_of :title, :author, :genre, :publication_year, message: '- Must not be empty!'
+  validates :title, :author, :genre, length: { minimum: 2 }
+  validates :publication_year, length: { maximum: 4 }, format: { with: /\A^[0-9]/x, message: '- Must be a year!' }
 end
