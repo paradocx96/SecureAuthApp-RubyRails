90fcbb48f9dd37d6143c890b6302e82ddc19985c
Navinda Chandrasiri <adilanga@gmail.com>
2023-06-21 11:40:13 +0530

    User Profile Impl

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
   get 'sign_up', to: 'registration#new'
   post 'sign_up', to: 'registration#create'
 
+  # User Routes
+  get 'profile', to: 'user#profile'
+
   # Root Route
   root 'home#index'
 end
